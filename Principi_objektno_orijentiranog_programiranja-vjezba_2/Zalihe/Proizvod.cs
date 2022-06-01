7ac5d8fe3230ef464bbbd398f493eba0b868b07d
karlaglazer <101133485+karlaglazer@users.noreply.github.com>
2022-06-02 00:02:43 +0200

    Add files via upload

---
+++ Principi_objektno_orijentiranog_programiranja-vjezba_2/Zalihe/Proizvod.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zalihe
+{
+    internal class Proizvod
+    {
+        public string Naziv { get; set; }
+        public double JedinicnaCijena { get; set; }
+        public double Stanje { get; set; }
+
+        public Proizvod()
+        {
+
+        }
+        public Proizvod(string naziv, double cijena, double stanje)
+        {
+            Naziv = naziv;  
+            JedinicnaCijena = cijena;
+            Stanje = stanje;
+        }
+        public double IzracunajVrijednostZaliha()
+        {
+            double vrijednost = Stanje * JedinicnaCijena;
+            return vrijednost;
+        }
+        public void DodajNaStanje(double kolicina)
+        {
+            if(kolicina > 0)
+            {
+                Stanje+=kolicina;
+            }
+        }
+        public void OduzmiSaStanja(double kolicina)
+        {
+            if(kolicina<0 || kolicina > Stanje)
+            {
+                Console.WriteLine("Ne moze");
+            }
+            else
+            {
+                Stanje-=kolicina;
+            }
+        }
+
+    }
+}
---
+++ Principi_objektno_orijentiranog_programiranja-vjezba_2/Zalihe/Skladiste.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zalihe
+{
+    internal class Skladiste
+    {
+        private List<Proizvod> listaProizvoda = new List<Proizvod>();
+        public void DodajProizvod(Proizvod proizvod)
+        {
+            bool postoji = false;
+            foreach(Proizvod p in listaProizvoda)
+            {
+                if(p.Naziv == proizvod.Naziv)
+                {
+                    postoji = true;
+                }
+            }
+            if (postoji == false)
+            {
+                listaProizvoda.Add(new Proizvod(proizvod.Naziv, proizvod.JedinicnaCijena, proizvod.Stanje));
+            }
+        }
+        public void UkloniProizvod(Proizvod proizvod)
+        {
+            foreach (Proizvod p in listaProizvoda)
+            {
+                if(p.Naziv == proizvod.Naziv)
+                {
+                    listaProizvoda.Remove(p);
+                }
+            }
+        }
+        public Proizvod DohvatiProizvod(string naziv)
+        {
+            Proizvod proizvod = null;
+            foreach(Proizvod p in listaProizvoda)
+            {
+                if(p.Naziv == naziv)
+                {
+                    proizvod = p;
+                }
+            }
+            return proizvod;
+        }
+        public void DohvatiSveProizvode()
+        {
+            foreach(Proizvod p in listaProizvoda)
+            {
+                Console.WriteLine($"Naziv: {p.Naziv}, jedinicna cijena: {p.JedinicnaCijena}, stanje: {p.Stanje}, vrijednost: {p.JedinicnaCijena * p.Stanje}");
+            }
+        }
+        public double IzracunajUkupnuVrijednostZaliha()
+        {
+            double vrijednost = 0;
+            foreach(Proizvod p in listaProizvoda)
+            {
+                double pojedinacno = p.JedinicnaCijena * p.Stanje;
+                vrijednost+=pojedinacno;
+            }
+            return vrijednost;
+        }
+    }
+}
