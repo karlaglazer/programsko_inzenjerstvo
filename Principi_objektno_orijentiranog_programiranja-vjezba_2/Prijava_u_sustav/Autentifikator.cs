c84746c2b0fe62cac4bfa43ac3b2f827777bed76
karlaglazer <101133485+karlaglazer@users.noreply.github.com>
2022-06-01 23:58:09 +0200

    Add files via upload

---
+++ Principi_objektno_orijentiranog_programiranja-vjezba_2/Prijava_u_sustav/Autentifikator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prijava_u_sustav
+{
+    internal class Autentifikator
+    {
+        private List<Korisnik> listaKorisnika;
+        public Autentifikator()
+        {
+            listaKorisnika = new List<Korisnik>();
+            listaKorisnika.Add(new Korisnik("pperic", "1111"));
+            listaKorisnika.Add(new Korisnik("iivic", "2222"));
+            listaKorisnika.Add(new Korisnik("jjuric", "3333"));
+        }
+        private Korisnik DohvatiKorisnika(string korisnickoIme)
+        {
+            Korisnik korisnik = null;
+            foreach(Korisnik k in listaKorisnika)
+            {
+                if(k.KorisnickoIme == korisnickoIme)
+                {
+                    korisnik = k;
+                }
+            }
+            return korisnik;
+        }
+        public string PrijaviKorisnika(string korisnickoIme, string lozinka)
+        {
+            string poruka = null;
+            Korisnik korisnik = DohvatiKorisnika(korisnickoIme);
+            if(korisnik == null)
+            {
+                poruka = "Ne postoji korisnik sa navedenim korisničkim imenom!";
+            }
+            else if(korisnik.Lozinka != lozinka)
+            {
+                poruka = "Pogrešno upisana lozinka!";
+            }
+            else
+            {
+                poruka = "Uspješno prijavljen korisnik!";
+            }
+            return poruka;
+        }
+    }
+}
---
+++ Principi_objektno_orijentiranog_programiranja-vjezba_2/Prijava_u_sustav/Korisnik.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prijava_u_sustav
+{
+    internal class Korisnik
+    {
+        public string KorisnickoIme { get; set; }
+        public string Lozinka { get; set; }
+        public Korisnik(string kIme, string lozinka)
+        {
+            KorisnickoIme = kIme;
+            Lozinka = lozinka;
+        }
+    }
+}
