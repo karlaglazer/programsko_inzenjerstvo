ebae2a7246ef0b876cf2150af3d3aef9294ee0ca
karlaglazer <101133485+karlaglazer@users.noreply.github.com>
2022-06-01 23:25:35 +0200

    Add files via upload

---
+++ Principi_objektno_orijentiranog_programiranja-vjezba_2/Hotelske_sobe/Hotel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotelske_sobe
+{
+    internal class Hotel
+    {
+        private List<Soba> soba;
+        public Hotel()
+        {
+            soba = new List<Soba>();
+            soba.Add(new Soba("10A", 3, StatusSobe.Slobodna));
+            soba.Add(new Soba("10B", 1, StatusSobe.Rezervirana));
+            soba.Add(new Soba("11A", 2, StatusSobe.Slobodna));
+            soba.Add(new Soba("11B", 4, StatusSobe.Slobodna));
+            soba.Add(new Soba("12A", 5, StatusSobe.Slobodna));
+            soba.Add(new Soba("12B", 3, StatusSobe.Rezervirana));
+            soba.Add(new Soba("13A", 1, StatusSobe.Slobodna));
+            soba.Add(new Soba("13B", 2, StatusSobe.Rezervirana));
+        }
+        public List<Soba> PronadjiSobe(int brojOsoba)
+        {
+            List<Soba> slobodne = new List<Soba>();
+            foreach(Soba s in soba)
+            {
+                if (s.Status == StatusSobe.Slobodna)
+                {
+                    if (s.Kapacitet >= brojOsoba)
+                    {
+                        slobodne.Add(s);
+                    }
+                }
+            }
+            return slobodne;
+        }
+        public void RezervirajSobu(string oznaka)
+        {
+            foreach(Soba s in soba)
+            {
+                if (s.Oznaka == oznaka)
+                {
+                    if (s.Status == StatusSobe.Slobodna)
+                    {
+                        s.Status=StatusSobe.Rezervirana;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Principi_objektno_orijentiranog_programiranja-vjezba_2/Hotelske_sobe/Soba.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotelske_sobe
+{
+    enum StatusSobe { Rezervirana, Slobodna }
+    internal class Soba
+    {
+        public string Oznaka { get; set; }
+        public int Kapacitet { get; set; }
+        public StatusSobe Status { get; set; }
+        public Soba(string oznaka, int kapacitet, StatusSobe status)
+        {
+            Oznaka = oznaka;
+            Kapacitet = kapacitet;
+            Status = status;
+        }
+    }
+}
