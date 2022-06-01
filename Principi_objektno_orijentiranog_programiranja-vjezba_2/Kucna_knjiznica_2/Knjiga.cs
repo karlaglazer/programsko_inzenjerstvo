0e26d72d87398d98c52cca4130eda4af4f64085f
karlaglazer <101133485+karlaglazer@users.noreply.github.com>
2022-06-01 23:30:23 +0200

    Add files via upload

---
+++ Principi_objektno_orijentiranog_programiranja-vjezba_2/Kucna_knjiznica_2/Knjiga.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kucna_knjiznica_2
+{
+    internal class Knjiga
+    {
+        public string Naslov { get; set; }
+        public bool Dostupno { get; set; }
+        public string Osoba { get; set; }
+        public Knjiga()
+        {
+
+        }
+        public Knjiga(string naslov)
+        {
+            Naslov = naslov;
+            Dostupno = true;
+            Osoba = null;
+        }
+        public string DohvatiOpis()
+        {
+            string opis = $"Naslov: {Naslov}, Dostupno: {Dostupno}, Osoba: {Osoba}";
+            return opis;
+        }
+    }
+}
---
+++ Principi_objektno_orijentiranog_programiranja-vjezba_2/Kucna_knjiznica_2/Knjiznica.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kucna_knjiznica_2
+{
+    internal class Knjiznica
+    {
+        public List<Knjiga> PopisKnjiga;
+        public Knjiznica()
+        {
+            PopisKnjiga = new List<Knjiga>();
+            PopisKnjiga.Add(new Knjiga("Prstenova druzina"));
+            PopisKnjiga.Add(new Knjiga("Dvije kule"));
+            PopisKnjiga.Add(new Knjiga("Povratak kralja"));
+        }
+        public void Posudi(string naslov, string osoba)
+        {
+            foreach(Knjiga k in PopisKnjiga)
+            {
+                if (k.Naslov == naslov)
+                {
+                    if (k.Dostupno == true)
+                    {
+                        k.Dostupno = false;
+                        k.Osoba = osoba;
+                    }
+                }
+            }
+        }
+    }
+}
