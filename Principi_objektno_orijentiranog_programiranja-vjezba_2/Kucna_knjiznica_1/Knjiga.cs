03ee70da49d0be5c6f28ecbef06884364af5340d
karlaglazer <101133485+karlaglazer@users.noreply.github.com>
2022-06-01 23:26:35 +0200

    Add files via upload

---
+++ Principi_objektno_orijentiranog_programiranja-vjezba_2/Kucna_knjiznica_1/Knjiga.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kucna_knjiznica_1
+{
+    enum StatusKnjige { Dostupno, NaPosudbi}
+    internal class Knjiga
+    {
+        public string ISBN { get; set; }
+        public string Naslov { get; set; }
+        public StatusKnjige Status { get; set; }
+        public Knjiga(string isbn, string naslov, StatusKnjige status)
+        {
+            ISBN = isbn;
+            Naslov = naslov;
+            Status = status;
+        }
+    }
+}
---
+++ Principi_objektno_orijentiranog_programiranja-vjezba_2/Kucna_knjiznica_1/Knjiznica.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kucna_knjiznica_1
+{
+    internal class Knjiznica
+    {
+        private List<Knjiga> knjige;
+        public Knjiznica()
+        {
+            knjige = new List<Knjiga>();
+            knjige.Add(new Knjiga("1111", "Prstenova druzina", StatusKnjige.Dostupno));
+            knjige.Add(new Knjiga("2222", "Dvije kule", StatusKnjige.Dostupno));
+            knjige.Add(new Knjiga("3333", "Povratak kralja", StatusKnjige.Dostupno));
+        }
+        public Knjiga DohvatiKnjigu(string isbn)
+        {
+            Knjiga knjiga = null;
+            foreach(Knjiga k in knjige)
+            {
+                if(k.ISBN == isbn)
+                {
+                    knjiga = k;
+                }
+            }
+            return knjiga;
+        }
+        public void Posudi(string isbn)
+        {
+            Knjiga knjiga = DohvatiKnjigu(isbn);
+            if(knjiga.Status == StatusKnjige.Dostupno)
+            {
+                knjiga.Status = StatusKnjige.NaPosudbi;
+            }
+        }
+    }
+}
