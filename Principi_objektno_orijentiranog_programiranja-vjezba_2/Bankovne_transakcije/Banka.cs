6840b9ac313d49cb57baaa24d3a0113482c05588
karlaglazer <101133485+karlaglazer@users.noreply.github.com>
2022-06-01 23:17:24 +0200

    Add files via upload

---
+++ Principi_objektno_orijentiranog_programiranja-vjezba_2/Bankovne_transakcije/Banka.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankovne_transakcije
+{
+    internal class Banka : Transakcija
+    {
+        private List<Racun> racuni;
+        public Banka()
+        {
+            racuni = new List<Racun>();
+            racuni.Add(new Racun("HR11", 100000));
+            racuni.Add(new Racun("HR22", 50000));
+            racuni.Add(new Racun("HR33", 12000));
+            racuni.Add(new Racun("HR44", 36000));
+        }
+        private Racun DohvatiRacun(string iban)
+        {
+            Racun racun = null;
+            foreach (Racun r in racuni)
+            {
+                if (iban == r.IBAN)
+                {
+                    racun = r;
+                }
+            }
+            return racun;
+        }
+        public Transakcija IzvrsiPlacanje(string platitelj, string primatelj, double iznos)
+        {
+            Transakcija transakcija = new Transakcija();
+            transakcija.Platitelj = DohvatiRacun(platitelj);
+            transakcija.Primatelj = DohvatiRacun(primatelj);
+            transakcija.Iznos = iznos;
+            transakcija.Platitelj.Stanje -= iznos;
+            transakcija.Primatelj.Stanje += iznos;
+           
+            return transakcija;
+
+            
+        }
+    }
+}
---
+++ Principi_objektno_orijentiranog_programiranja-vjezba_2/Bankovne_transakcije/Racun.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankovne_transakcije
+{
+    internal class Racun
+    {
+        public string IBAN { get; set; }
+        public double Stanje { get; set; }
+
+        public Racun (string iban, double stanje)
+        {
+            IBAN = iban;
+            Stanje = stanje;
+        }
+    }
+}
