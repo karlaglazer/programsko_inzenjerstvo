f1befa67be3ad3d732a3aa661af2b34ea180b2d9
karlaglazer <101133485+karlaglazer@users.noreply.github.com>
2022-06-02 00:01:02 +0200

    Add files via upload

---
+++ Principi_objektno_orijentiranog_programiranja-vjezba_2/Senzori/MeteoStanica.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senzori_1
+{
+    internal class MeteoStanica
+    {
+        private List<Senzor> senzori;
+        public MeteoStanica()
+        {
+            senzori = new List<Senzor>();
+            senzori.Add(new Senzor("Senzor 1", 8.4, JedinicaMjere.Celzius));
+            senzori.Add(new Senzor("Senzor 2", 281.5, JedinicaMjere.Kelvin));
+            senzori.Add(new Senzor("Senzor 3", 8.9, JedinicaMjere.Celzius));
+            senzori.Add(new Senzor("Senzor 4", 9.1, JedinicaMjere.Celzius));
+        }
+        private double Pretvori(JedinicaMjere izJedinice, double vrijednost)
+        {
+            double pretvori = 0;
+            if (izJedinice == JedinicaMjere.Celzius)
+            {
+                pretvori = vrijednost + 273.15;
+            }
+            else if(izJedinice == JedinicaMjere.Kelvin)
+            {
+                pretvori = vrijednost - 273.15;
+            }
+            return pretvori;
+        }
+        public double DohvatiProsjecnuTemperaturu(JedinicaMjere jedinica)
+        {
+            double zbroj = 0;
+            foreach(Senzor s in senzori)
+            {
+                if(s.Jedinica == jedinica)
+                {
+                    zbroj += s.Vrijednost;
+                }
+                else
+                {
+                    double pretvoreno = Pretvori(s.Jedinica, s.Vrijednost);
+                    zbroj += pretvoreno;
+                }
+            }
+            double prosjecna = zbroj / senzori.Count;
+            return prosjecna;
+        }
+    }
+}
---
+++ Principi_objektno_orijentiranog_programiranja-vjezba_2/Senzori/Senzor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senzori_1
+{
+    enum JedinicaMjere { Celzius, Kelvin}
+    internal class Senzor
+    {
+        public string Oznaka { get; set; }
+        public double Vrijednost { get; set; }
+        public JedinicaMjere Jedinica { get; set; }
+        public Senzor(string oznaka, double vrijednost, JedinicaMjere jedinica)
+        {
+            Oznaka = oznaka;
+            Vrijednost = vrijednost;
+            Jedinica = jedinica;
+        }
+    }
+}
