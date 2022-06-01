cabe7f1fa5729acbd9d4ef6b12f112f104562cdf
karlaglazer <101133485+karlaglazer@users.noreply.github.com>
2022-06-01 23:15:57 +0200

    Add files via upload

---
+++ Principi_objektno_orijentiranog_programiranja-vjezba_2/Autobusna_karta/Linija.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autobusna_karta
+{
+    internal class Linija
+    {
+        public string Polaziste { get; set; }
+        public string Odrediste { get; set; }
+        public int Udaljenost { get; set; }
+        public Linija(string polaziste, string odrediste, int udaljenost)
+        {
+            Polaziste = polaziste;
+            Odrediste = odrediste;
+            Udaljenost = udaljenost;
+        }
+
+    }
+}
---
+++ Principi_objektno_orijentiranog_programiranja-vjezba_2/Autobusna_karta/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autobusna_karta
+{
+    internal class Program 
+    {
+        static AutobusniKolodvor autobusniKolodvor = new AutobusniKolodvor();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite polaziste, odrediste i tip karte:");
+            string unos = Console.ReadLine();
+            string[] polje = unos.Split(' ');
+
+            autobusniKolodvor.KupiKartu(polje[0], polje[1], polje[2]);
+
+            Console.ReadLine();
+        }
+    }
+}
