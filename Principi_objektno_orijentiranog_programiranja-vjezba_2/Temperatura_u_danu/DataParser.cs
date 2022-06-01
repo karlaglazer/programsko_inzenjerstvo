2bc66ddbd640dc7735149b74df2d0b4ac3effbb5
karlaglazer <101133485+karlaglazer@users.noreply.github.com>
2022-06-02 00:02:04 +0200

    Add files via upload

---
+++ Principi_objektno_orijentiranog_programiranja-vjezba_2/Temperatura_u_danu/DataParser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperatura_u_danu
+{
+    internal class DataParser
+    {
+        List<double> listaTemperatura = new List<double>();
+        public List<double> Parse(string sensorData)
+        {
+            List<string> data = new List<string>();
+            data = sensorData.Split(';').ToList();
+            listaTemperatura = data.Select(x=>double.Parse(x)).ToList();
+            return listaTemperatura;
+        }
+        
+    }
+}
---
+++ Principi_objektno_orijentiranog_programiranja-vjezba_2/Temperatura_u_danu/Reporter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperatura_u_danu
+{
+    internal class Reporter
+    {
+        static Sensor sensor = new Sensor();
+        static DataParser dataParser = new DataParser();
+        private double GetMinimalTemperature(List<double> lista)
+        {
+            return lista.Min();
+        }
+        private double GetMaximalTemperature(List<double> lista)
+        {
+            return lista.Max();
+        }
+        private double GetAverageTemperature(List<double> lista)
+        {
+            double zbroj = 0;
+            foreach(double p in lista)
+            {
+                zbroj += p;
+            }
+            double prosjecna = zbroj / (double)lista.Count;
+            return prosjecna;
+        }
+        public string GenerateReport()
+        {
+            List<double> lista = new List<double>();
+            string podaci = sensor.GetData();
+            lista = dataParser.Parse(podaci);
+            return $"Prosjecna temperatura je {GetAverageTemperature(lista)} C, minimalna temperatura je {GetMinimalTemperature(lista)} C, maksimalna temperatura je {GetMaximalTemperature(lista)} C";
+        }
+    }
+}
