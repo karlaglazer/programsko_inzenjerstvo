b277d12dd3dd43e060df0bd1ac58a579a298c0ae
karlaglazer <101133485+karlaglazer@users.noreply.github.com>
2022-06-02 19:32:55 +0200

    Add files via upload

---
+++ UML_dijagram_aktivnosti_i_slijeda.vjezba_6/Dionice/Stock.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dionice
+{
+    internal class Stock
+    {
+        public string Name { get; set; }
+        public double CurrentPrice { get; set; }
+        public double PreviousPrice { get; set; }
+        public double Difference { get; set; }
+
+        public Stock(string name, double currentPrice, double previousPrice)
+        {
+            Name = name;
+            CurrentPrice = currentPrice;
+            PreviousPrice = previousPrice;
+            Difference = Math.Round(((CurrentPrice - PreviousPrice) / PreviousPrice) * 100,2);
+        }
+    }
+}
---
+++ UML_dijagram_aktivnosti_i_slijeda.vjezba_6/Dionice/StockManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StocksLib;
+
+namespace Dionice
+{
+    internal class StockManager
+    {
+        public List<Stock> GetStocks()
+        {
+            List<string> stocksData = StockExchangeAPI.GetStocksData();
+            List<Stock> stocks = new List<Stock>();
+            foreach (string stock in stocksData)
+            {
+                stocks.Add(StockParser.ParseStock(stock));
+            }
+            return stocks;
+        }
+    }
+}
