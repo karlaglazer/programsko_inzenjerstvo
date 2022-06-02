4b5b949d1bb95982e5a667ea95c047b62a47888b
karlaglazer <101133485+karlaglazer@users.noreply.github.com>
2022-06-02 19:23:09 +0200

    Create Dvorana.cs

---
+++ UML_dijagram_klasa-koncepti_i_implementacija-vjezba_5/Kino_2/Dvorana.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kino2
+{
+    internal class Dvorana
+    {
+        public int Oznaka { get; set; }
+        public List<Sjedalo> ListaSjedala { get; set; }
+    }
+}
